2bf15bed03c98b385e9b4cddadf96046dc4b3a70
aice030 <cheng000811@gmail.com>
2026-08-12 00:37:16 +0800

    创建序列化接口

---
+++ rpc-core/src/main/java/com/aice/rpc/serialize/Serializer.java
@@ -0,0 +1,18 @@
+package com.aice.rpc.serialize;
+
+/**
+ * @author aice Cheng
+ * Created on  2026/8/11 23:53
+ */
+public interface Serializer {
+
+    /**
+     * 序列化
+     * */
+    byte[] serialize(Object object);
+
+    /**
+     * 反序列化
+     * */
+    <T> T deserialize(byte[] bytes, Class<T> targetClass);
+}
