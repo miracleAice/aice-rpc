c848bebcd4b76b1e17b4dcdf8d4561e1c402c05f
aice030 <cheng000811@gmail.com>
2026-09-25 16:33:23 +0800

    让 ServiceInstance 为创建后就不可变的服务实例

--- rpc-core/src/main/java/com/aice/rpc/registry/ServiceInstance.java
+++ rpc-core/src/main/java/com/aice/rpc/registry/ServiceInstance.java
@@ -1,6 +1,6 @@
 package com.aice.rpc.registry;
 
-import lombok.Data;
+import lombok.Getter;
 
 /**
  * 服务实例
@@ -8,8 +8,26 @@ import lombok.Data;
  * @author aice Cheng
  * Created on  2026/9/25 14:15
  */
-@Data
+@Getter
 public class ServiceInstance {
-    private String host;
-    private int port;
+    private final String host;
+    private final int port;
+
+    /**
+     * 创建不可变的服务实例。
+     *
+     * @param host 服务地址
+     * @param port 服务端口
+     */
+    public ServiceInstance(String host, int port) {
+        if (host == null || host.isBlank()) {
+            throw new IllegalArgumentException("服务地址不能为空");
+        }
+        if (port < 1 || port > 65535) {
+            throw new IllegalArgumentException("服务端口不合法");
+        }
+
+        this.host = host;
+        this.port = port;
+    }
 }
